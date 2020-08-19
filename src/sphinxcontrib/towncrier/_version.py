e6ff4683835de928a50f1b7bb769d84cec271c26
slsh1o <slsh1o-git@protonmail.com>
2020-08-19 20:36:27 +0300

    Add flake8's wemake plugin & fix its errors

--- src/sphinxcontrib/towncrier/_version.py
+++ src/sphinxcontrib/towncrier/_version.py
@@ -2,7 +2,7 @@
 
 try:
     # pylint: disable=unused-import
-    from ._scm_version import version as __version__
+    from ._scm_version import version as __version__  # noqa: WPS433, WPS436
 except ImportError:
-    from pkg_resources import get_distribution as _get_dist
-    __version__ = _get_dist('sphinxcontrib-towncrier').version
+    from pkg_resources import get_distribution as _get_dist  # noqa: WPS433
+    __version__ = _get_dist('sphinxcontrib-towncrier').version  # noqa: WPS440
