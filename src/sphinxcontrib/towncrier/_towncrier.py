952ea8a3a66d4da355a45657bec6da0bcdcd378c
q0w <43147888+q0w@users.noreply.github.com>
2022-12-22 00:06:38 +0300

    👽 Normalize Towncrier config object as a dict
    This is a compatibility change needed for Towncrier >= 22.12.0rc1
    after it changed the internal object to be a `dataclass`.
    
    Refs:
    * https://github.com/twisted/towncrier/pull/429
    * https://github.com/twisted/towncrier/issues/461
    
    Resolves #76.
    
    Co-Authored-By: davfsa <davfsa@gmail.com>
    Co-Authored-By: Sviatoslav Sydorenko <webknjaz@redhat.com>

--- src/sphinxcontrib/towncrier/_towncrier.py
+++ src/sphinxcontrib/towncrier/_towncrier.py
@@ -1,7 +1,14 @@
 """Towncrier related shims."""
 
+from contextlib import suppress as _suppress_exception
 from pathlib import Path
 from typing import Any, Dict
+
+
+with _suppress_exception(ImportError):
+    # NOTE: This will not raise an exception under Python >= 3.7, and is only
+    # NOTE: needed for Towncrier >= 22.12.0rc1 which doesn't support Python 3.6
+    from dataclasses import asdict as _dataclass_to_dict  # noqa: WPS433
 
 
 try:
@@ -24,7 +31,15 @@ def get_towncrier_config(
     """Return the towncrier config dictionary."""
     try:
         # Towncrier >= 19.9.0
-        return load_config_from_file(str(project_path), str(final_config_path))
+        config = load_config_from_file(
+            str(project_path), str(final_config_path),
+        )
     except TypeError:
         # Towncrier < 19.9.0
         return load_config_from_file(str(final_config_path))
+
+    if isinstance(config, dict):
+        # Towncrier < 22.12.0rc1
+        return config
+
+    return _dataclass_to_dict(config)
