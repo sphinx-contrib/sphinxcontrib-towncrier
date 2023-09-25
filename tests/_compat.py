91d50746a1fa04f0782d50bd2a5be79d160baca0
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2023-09-25 03:17:27 +0200

    Make _towncrier_test compatible w/ Towncrier 19.2

--- tests/_compat.py
+++ tests/_compat.py
@@ -2,10 +2,17 @@
 
 
 try:
+    # Towncrier >= 22.8.0rc1
     from towncrier._settings.load import (  # noqa: WPS433
         ConfigError as TowncrierConfigError,
     )
 except ImportError:
-    from towncrier._settings import (  # noqa: F401, WPS433, WPS440
-        ConfigError as TowncrierConfigError,
-    )
+    # Towncrier < 22.8.0rc1
+    try:  # noqa: WPS505
+        # Towncrier >= 19.9.0
+        from towncrier._settings import (  # noqa: WPS433, WPS440
+            ConfigError as TowncrierConfigError,
+        )
+    except ImportError:
+        # Towncrier < 19.9.0
+        TowncrierConfigError = ValueError  # noqa: WPS440
