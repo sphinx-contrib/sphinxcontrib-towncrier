73ad03692fa4b1a8c4d50b401ab7f9b385c2ba3a
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2024-12-23 07:10:09 +0100

    📝 Correct the `get_towncrier_config` docstring
    Post refactor.

--- src/sphinxcontrib/towncrier/_towncrier.py
+++ src/sphinxcontrib/towncrier/_towncrier.py
@@ -12,5 +12,5 @@ def get_towncrier_config(
         project_path: Path,
         final_config_path: Union[Path, None],
 ) -> Config:
-    """Return the towncrier config dictionary."""
+    """Return the towncrier config in native format."""
     return load_config_from_file(str(project_path), str(final_config_path))
