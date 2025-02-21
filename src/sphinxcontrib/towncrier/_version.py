609ecde98b1b882e54d0647a8360dc42e2eef8fb
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2025-02-21 13:44:45 +0100

    Hide missing `_scm_version` branch @ test coverage

--- src/sphinxcontrib/towncrier/_version.py
+++ src/sphinxcontrib/towncrier/_version.py
@@ -3,7 +3,7 @@
 try:
     # pylint: disable=unused-import
     from ._scm_version import version as __version__  # noqa: WPS433, WPS436
-except ImportError:
+except ImportError:  # pragma: no cover  # difficult to hit in tests
     from importlib.metadata import (  # noqa: WPS433, WPS436
         version as importlib_metadata_get_version,
     )
