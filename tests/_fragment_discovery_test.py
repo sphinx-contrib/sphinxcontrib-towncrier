94c4bdc041a497686abb0edbcc57b1c3ea2de939
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2025-02-24 18:50:03 +0100

    🧪 Cover `lookup_towncrier_fragments` with tests

--- tests/_fragment_discovery_test.py
+++ tests/_fragment_discovery_test.py
@@ -75,6 +75,56 @@ def test_resolve_spec_config(
     assert _resolve_spec_config(base_path, config_path) == resolved_path
 
 
+@pytest.mark.parametrize(
+    'chdir',
+    (True, False),
+    ids=('cwd', 'detached-work-dir'),
+)
+@pytest.mark.parametrize(
+    ('sphinx_configured_path', 'config_file_path'),
+    (
+        (None, TOWNCRIER_TOML_FILENAME),
+        (None, PYPROJECT_TOML_FILENAME),
+        (TOWNCRIER_TOML_FILENAME, TOWNCRIER_TOML_FILENAME),
+        (PYPROJECT_TOML_FILENAME, PYPROJECT_TOML_FILENAME),
+    ),
+    ids=(
+        'implicit-config-path-pyproject',
+        'explicit-config-path-pyproject',
+        'implicit-config-path-towncrier',
+        'explicit-config-path-towncrier',
+    ),
+)
+def test_lookup_towncrier_fragments(
+        chdir: bool,
+        config_file_path: str,
+        monkeypatch: pytest.MonkeyPatch,
+        sphinx_configured_path: Union[str, None],
+        tmp_path: Path,
+) -> None:
+    """Test that fragments can be discovered in configured location."""
+    change_notes_dir_base_name = 'newsfragments-sentinel'
+    tmp_working_dir_path = tmp_path / 'working-directory'
+    change_notes_dir_path = tmp_working_dir_path / change_notes_dir_base_name
+    change_note_sentinel_path = change_notes_dir_path / '0.misc.1.rst'
+
+    change_notes_dir_path.mkdir(parents=True)
+    change_note_sentinel_path.write_text('sentinel', encoding='utf-8')
+
+    (tmp_working_dir_path / config_file_path).write_text(
+        f'[tool.towncrier]\ndirectory={change_notes_dir_base_name !r}',
+        encoding='utf-8',
+    )
+
+    if chdir:
+        monkeypatch.chdir(tmp_working_dir_path)
+    discovered_fragment_paths = lookup_towncrier_fragments.__wrapped__(
+        None if chdir else tmp_working_dir_path,
+        sphinx_configured_path,
+    )
+    assert discovered_fragment_paths == {change_note_sentinel_path}
+
+
 def test_lookup_towncrier_fragments_missing_cfg(tmp_path: Path) -> None:
     """Test that missing config file causes zero fragment set."""
     discovered_fragment_paths = lookup_towncrier_fragments.__wrapped__(
