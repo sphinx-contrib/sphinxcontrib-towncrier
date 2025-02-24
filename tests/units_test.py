89f255ae1c4583ace85f6b18856a308be0dec1de
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2025-02-25 00:48:19 +0100

    🧪 Fully cover _get_changelog_draft_entries

--- tests/units_test.py
+++ tests/units_test.py
@@ -1,5 +1,6 @@
 """Unit tests of the extension bits."""
 
+import json
 import shlex
 import sys
 import typing
@@ -10,6 +11,7 @@ from sphinxcontrib.towncrier.ext import _get_changelog_draft_entries
 
 
 NO_OUTPUT_MARKER = r'\[No output\]'
+PYTHON_INLINE_SNIPPET_CMD_FLAG = '-c'
 
 
 _get_changelog_draft_entries_unwrapped = (
@@ -31,7 +33,7 @@ _get_changelog_draft_entries_unwrapped = (
         (
             (
                 sys.executable,
-                '-c',
+                PYTHON_INLINE_SNIPPET_CMD_FLAG,
                 r'print("test standard output\nsecond line");'
                 'raise SystemExit(1)',
             ),
@@ -41,7 +43,7 @@ _get_changelog_draft_entries_unwrapped = (
         (
             (
                 sys.executable,
-                '-c',
+                PYTHON_INLINE_SNIPPET_CMD_FLAG,
                 r'raise SystemExit("test standard error\nsecond line")',
             ),
             NO_OUTPUT_MARKER,
@@ -50,7 +52,7 @@ _get_changelog_draft_entries_unwrapped = (
         (
             (
                 sys.executable,
-                '-c',
+                PYTHON_INLINE_SNIPPET_CMD_FLAG,
                 r'print("test standard output\nsecond line out");'
                 r'raise SystemExit("test standard error\nsecond line err")',
             ),
@@ -98,3 +100,51 @@ def test_towncrier_draft_generation_failure_msg(
 
     with pytest.raises(RuntimeError, match=expected_error_message):
         _get_changelog_draft_entries_unwrapped(version_string)
+
+
+def test_towncrier_draft_generation_with_config(
+        monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """Test explicit config gets passed into Towncrier."""
+    monkeypatch.setattr(
+        'sphinxcontrib.towncrier.ext.TOWNCRIER_DRAFT_CMD',
+        (
+            sys.executable,
+            '-I',
+            PYTHON_INLINE_SNIPPET_CMD_FLAG,
+            'import json, sys; print(json.dumps(sys.argv))',
+        ),
+    )
+
+    argv_json = _get_changelog_draft_entries_unwrapped(
+        'test version',
+        config_path='sentinel-config-path',
+    )
+    computed_proc_args = json.loads(argv_json)
+    computed_towncrier_args = computed_proc_args[3:]
+
+    assert computed_towncrier_args == ['--config', 'sentinel-config-path']
+
+
+def test_towncrier_draft_generation_with_empty_cl(
+        monkeypatch: pytest.MonkeyPatch,
+) -> None:
+    """Test that empty change log triggers an exception."""
+    monkeypatch.setattr(
+        'sphinxcontrib.towncrier.ext.TOWNCRIER_DRAFT_CMD',
+        (
+            sys.executable,
+            '-I',
+            PYTHON_INLINE_SNIPPET_CMD_FLAG,
+            'print("No significant changes")',
+        ),
+    )
+
+    expected_error_message = (
+        '^There are no unreleased changelog entries so far$'
+    )
+    with pytest.raises(LookupError, match=expected_error_message):
+        _get_changelog_draft_entries_unwrapped(
+            'test version',
+            allow_empty=False,
+        )
